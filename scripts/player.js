66a88d6b9b67d77f1431bd9476f24a8e2621e8f7
Willem Deen <hi@willemdeen.com>
2020-03-27 00:54:34 +0100

    [feat] New player layout

---
+++ scripts/player.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() { initialiseMediaPlayer(); }, false);
+
+var mediaPlayer;
+
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('media-video');
+   mediaPlayer.controls = false;
+}
+
+function togglePlayPause() {
+   var btn = document.getElementById('play-pause-button');
+   if (mediaPlayer.paused || mediaPlayer.ended) {
+      btn.title = 'pause';
+      btn.innerHTML = 'pause';
+      btn.className = 'pause';
+      mediaPlayer.play();
+   }
+   else {
+      btn.title = 'play';
+      btn.innerHTML = 'play';
+      btn.className = 'play';
+      mediaPlayer.pause();
+   }
+}
+
+function changeButtonType(btn, value) {
+   btn.title = value;
+   btn.innerHTML = value;
+   btn.className = value;
+}
+
+function stopPlayer() {
+   mediaPlayer.pause();
+   mediaPlayer.currentTime = 0;
+}
+
+function updateSlider(value) {
+   console.log(value/100);
+   mediaPlayer.volume = parseFloat(value/100).toFixed(1);
+}
